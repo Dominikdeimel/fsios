482f29c81ff23b74246a9a2bbff09a70259c1cf3
Dominik Deimel <domi.deimel@yahoo.de>
2022-03-01 11:40:46 +0100

    Added ViewModel and Model

---
+++ Drawing.io/Drawing.io/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  Drawing.io
+//
+//  Created by Dominik Deimel on 01.03.22.
+//
+
+import Foundation
---
+++ Drawing.io/Drawing.io/ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModel.swift
+//  Drawing.io
+//
+//  Created by Dominik Deimel on 01.03.22.
+//
+
+import Foundation
