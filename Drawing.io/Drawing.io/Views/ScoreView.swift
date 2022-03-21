a76f9c6410fdaa6a2be62ae4afb7fc6e20dd9e13
AnleAnja <anja_katharina.bertels@smail.th-koeln.de>
2022-03-21 14:10:16 +0100

    add score view for success

--- Drawing.io/Drawing.io/Views/ScoreView.swift
+++ Drawing.io/Drawing.io/Views/ScoreView.swift
@@ -8,13 +8,36 @@
 import SwiftUI
 
 struct ScoreView: View {
+    @ObservedObject var viewModel: ViewModel
+    @State var counter: Int = 1
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            VStack {
+                Text(viewModel.given)
+                Image(uiImage: viewModel.image)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .border(.gray)
+                Text("Score: x")
+                Text("Gesamtscore: y")
+                ConfettiCannon(counter: $counter)
+            }
+            .padding()
+            .onTapGesture(){
+                counter += 1
+            }
+            ConfettiCannon(counter: $counter)
+        }
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                counter += 1
+            }
+        }
     }
 }
 
-struct ScoreView_Previews: PreviewProvider {
-    static var previews: some View {
-        ScoreView()
-    }
-}
+//struct ScoreView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ScoreView()
+//    }
+//}
